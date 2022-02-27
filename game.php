b355d41d962223b92ff0bc45b5223a4a5251ed1f
Axel De Roeck <axelderoeck22@gmail.com>
2022-02-27 17:35:43 +0100

    add pawns from array

--- game.php
+++ game.php
@@ -11,6 +11,7 @@ $team = 'blue';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>Document</title>
 </head>
 <body id="fullboard">
@@ -32,6 +33,42 @@ $team = 'blue';
     </div>
 
 <p>Current theme: <?=$theme?></p>
+
+<script>
+
+    /* Pawns 2D array = [[X, Y, Pawn, Team], ...]
+    *
+    * @param pawns[ID][0] = X (1-10)
+    * @param pawns[ID][1] = Y (1-10)
+    * @param pawns[ID][2] = Pawn (0-11) // Spy = 0, Bomb = 10, Flag = 11
+    * @param pawns[ID][3] = Team (0/1) // Blue = 0, Red = 1
+    * 
+    **/
+
+    let pawns = [
+        [10, 10, 11, 0],
+        [10, 9, 10, 0],
+        [9, 10, 10, 0],
+        [10, 7, 9, 0],
+        [2, 2, 2, 0]
+    ];
+
+    const placePawns = () => {
+        // Delete old images
+        $("#board div")
+            .children()
+            .remove();
+        
+        // Place pawns
+        pawns.forEach(pawn => {
+            $("div[data-x='" + pawn[0] + "'][data-y='" + pawn[1] + "']")
+                .prepend('<img src="./themes/<?=$theme?>/' + pawn[3] + '/' + pawn[2] + '.svg" />')
+        })
+    }
+
+    placePawns();
+
+</script>
     
 </body>
 </html>
