0f6c22e3e08b88fd376f99065cd94666306a0550
Axel De Roeck <axelderoeck22@gmail.com>
2022-02-27 17:28:54 +0100

    add board styling

--- game.php
+++ game.php
@@ -1,5 +1,7 @@
 <?php 
 $theme = $_GET['t'];
+$unusable_tiles = array(43, 44, 53, 54, 47, 48, 57, 58);
+$team = 'blue';
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +10,26 @@ $theme = $_GET['t'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
-<body>
+<body id="fullboard">
+
+    <div id="board">
+        <?php
+        $tile = 1;
+        for($y = 1; $y <= 10; $y++){
+            for($x = 1; $x <= 10; $x++){
+                if (in_array($tile, $unusable_tiles)){
+                    echo "<div class='unusable' data-x=$x data-y=$y data-tile=$tile></div>";
+                }else{
+                    echo "<div data-x=$x data-y=$y data-tile=$tile></div>";
+                }
+                $tile++;
+            }
+        } 
+        ?>
+    </div>
 
 <p>Current theme: <?=$theme?></p>
     
