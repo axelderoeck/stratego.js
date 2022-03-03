38fa74dc28740a2fc5e4dd080ef89f148980b8d6
Axel De Roeck <axelderoeck22@gmail.com>
2022-03-03 17:12:24 +0100

    make config file per theme

--- public/js/stratego.js
+++ public/js/stratego.js
@@ -1,23 +1,7 @@
 // Connect to socket
 let socket = io();
 
-// Size of both sides of the board, size 10 = 100 tiles
-const BOARD_SIZE = 10;
-// Coordinates of disabled tiles
-const DISABLED_TILES = [
-    // X, Y
-    // Left square
-    [3, 5],
-    [4, 5],
-    [3, 6],
-    [4, 6],
-    // Right square
-    [7, 5],
-    [8, 5],
-    [7, 6],
-    [8, 6]
-]
-
+// Length of roomcode
 const ROOMCODE_LENGTH = 6;
 
 let player = {
---
+++ public/themes/classic/config.js
@@ -0,0 +1,16 @@
+// Size of both sides of the board, size 10 = 100 tiles
+const BOARD_SIZE = 10;
+// Coordinates of disabled tiles
+const DISABLED_TILES = [
+    // X, Y
+    // Left square
+    [3, 5],
+    [4, 5],
+    [3, 6],
+    [4, 6],
+    // Right square
+    [7, 5],
+    [8, 5],
+    [7, 6],
+    [8, 6]
+]
